857f01ab1fb6dd6da5146a082b330861a2c41118
viksi01 <victoria.pyatnicka@gmail.com>
2022-11-25 01:25:27 +0200

    fcommit

---
+++ UnitTest_6.4.rec/UnitTest_6.4.rec.cpp
@@ -0,0 +1,26 @@
+﻿#include "pch.h"
+#include "CppUnitTest.h"
+#include "../lab_6.4.rec/lab_6.4.rec.cpp"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace UnitTest64rec
+{
+	TEST_CLASS(UnitTest64rec)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+			double* a = new double[5];
+			a[0] = -3.;
+			a[1] = 8.9;
+			a[2] = 30.;
+			a[3] = -5.;
+			a[4] = 45.;
+
+			double p = Product(a, 5, 1., 0);
+			Assert::AreEqual(p, 15.);
+		}
+	};
+}
---
+++ lab_6.4.rec/lab_6.4.rec.cpp
@@ -0,0 +1,71 @@
+﻿#include <iostream>
+#include <time.h>
+
+using namespace std;
+
+void Create(double*& a,  int n, int i)
+{
+	a[i] = -100. + rand() * (100. + 100.) / RAND_MAX;
+	if (i < n - 1)
+		Create(a, n, i + 1);
+
+}
+
+void Print(double*& a, int n, int i)
+{
+	cout << a[i] << " , ";
+	if (i < n - 1)
+
+		Print(a, n, i + 1);
+	else
+
+		return;
+}
+
+double Product(double*& a, int n, double p, int i)
+{
+	if (a[i] < 0)
+	{
+		p *= a[i];
+	}
+	if (i < n - 1)
+		return Product(a, n, p, i + 1);
+	else
+		return p;
+
+}
+
+void Inverse(double*& a, int n, int i)
+{
+	double tmp = a[i];
+	a[i] = a[n - 1 - i];
+	a[n - 1 - i] = tmp;
+	if (i < n / 2 - 1)
+		Inverse(a, n, i + 1);
+}
+
+void Print_Inverse(double*& a, int n, int i)
+{
+	cout << a[i] << " , ";
+	if (i < n - 1)
+
+		Print(a, n, i + 1);
+	else
+
+		return;
+}
+
+int main()
+{
+	srand((unsigned)time(NULL));
+    int n;
+	cout << "n = "; cin >> n;
+	double* a = new double[n];
+	Create(a, n, 0);
+	cout << "a {"; Print(a, n, 0); cout << "}" << endl;
+	cout << "1.1) P = " << Product(a, n, 1., 0) << endl;
+	Inverse(a, n, 0);
+	cout << "2) Inverse a{"; Print_Inverse(a, n, 0); cout << "}" << endl;
+
+	delete[]a;
+}
